7f738843309a6bd9ada949f4f566d6aeb7f07f9f
YuryKravchenko <yurezz08@mail.ru>
2024-02-13 00:21:04 +0300

    execution

--- src/main/java/com/iglaz/astonbase/lesson5/homework/PuttingIntoPractice.java
+++ src/main/java/com/iglaz/astonbase/lesson5/homework/PuttingIntoPractice.java
@@ -1,7 +1,9 @@
 package com.iglaz.astonbase.lesson5.homework;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PuttingIntoPractice {
 
@@ -19,5 +21,79 @@ public class PuttingIntoPractice {
                 new Transaction(mario, 2012, 700),
                 new Transaction(alan, 2012, 950)
         );
+
+        //1. Найти все транзакции за 2011 год и отсортировать их по сумме (от меньшей к большей).
+        System.out.println("Все транзакции за 2011 год:");
+        transactions
+                .stream()
+                .filter(transaction -> transaction.getYear() == 2011)
+                .sorted(Comparator.comparing(Transaction::getValue))
+                .forEach(System.out::println);
+        System.out.println("------");
+
+        //2. Вывести список неповторяющихся городов, в которых работают трейдеры.
+        System.out.println("Список городов, в которых работают трейдеры:");
+        transactions
+                .stream()
+                .map(transaction -> transaction.getTrader().getCity())
+                .distinct()
+                .forEach(System.out::println);
+        System.out.println("------");
+
+        //3. Найти всех трейдеров из Кембриджа и отсортировать их по именам.
+        System.out.println("Трейдеры из кембриджа:");
+        transactions
+                .stream()
+                .map(Transaction::getTrader)
+                .filter(trader -> trader.getCity().equals("Cambridge"))
+                .sorted(Comparator.comparing(Trader::getName))
+                .distinct()
+                .forEach(System.out::println);
+        System.out.println("------");
+
+        //4. Вернуть строку со всеми именами трейдеров, отсортированными в алфавитном порядке.
+        System.out.println("Строка с именами трейдеров:");
+        String tradersNamesLine = transactions
+                .stream()
+                .map(transaction -> transaction.getTrader().getName())
+                .distinct()
+                .sorted()
+                .collect(Collectors.joining());
+        System.out.println(tradersNamesLine);
+        System.out.println("------");
+
+        //5. Выяснить, существует ли хоть один трейдер из Милана.
+        boolean isAnyTraderFromMilan = transactions
+                .stream()
+                .anyMatch(transaction -> transaction.getTrader().getCity().equals("Milan"));
+        System.out.println(isAnyTraderFromMilan ? "Есть трейдер из Милана." : "Нет трейдера из Милана.");
+        System.out.println("------");
+
+        //6. Вывести суммы всех транзакций трейдеров из Кембриджа.
+        System.out.println("Сумма всех транзакций трейдеров из Кембриджа:");
+        System.out.println(transactions
+                .stream()
+                .filter(transaction -> transaction.getTrader().getCity().equals("Cambridge"))
+                .mapToInt(Transaction::getValue)
+                .sum());
+        System.out.println("------");
+
+        //7. Какова максимальная сумма среди всех транзакций?
+        System.out.println("Транзакция с максимальной суммой:");
+        System.out.println(transactions
+                .stream()
+                .mapToInt(Transaction::getValue)
+                .max()
+                .orElse(0));
+        System.out.println("------");
+
+        //8. Найти транзакцию с минимальной суммой.
+        System.out.println("Транзакция с минимальной суммой:");
+        System.out.println(transactions
+                .stream()
+                .mapToInt(Transaction::getValue)
+                .min()
+                .orElse(0));
+
     }
 }
